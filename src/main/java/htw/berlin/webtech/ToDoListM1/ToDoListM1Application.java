e1d4017e65f339a2f2bc367471defef9a4d72a8e
Sinan Ertogrul <sinanerto@MBP-von-Sinan.fritz.box>
2021-10-20 18:04:34 +0200

    first commit

---
+++ src/main/java/htw/berlin/webtech/ToDoListM1/ToDoListM1Application.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.ToDoListM1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListM1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListM1Application.class, args);
+	}
+
+}
---
+++ src/test/java/htw/berlin/webtech/ToDoListM1/ToDoListM1ApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.ToDoListM1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListM1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
